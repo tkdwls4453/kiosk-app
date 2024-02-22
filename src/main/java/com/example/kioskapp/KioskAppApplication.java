270e59742465e0a79ef2890d6bbd4373ccca0a09
Yoon321 <tkdwls734@gmail.com>
2024-02-22 21:54:27 +0900

    initial setting

---
+++ src/main/java/com/example/kioskapp/KioskAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.kioskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KioskAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kioskapp/KioskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kioskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
