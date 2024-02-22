9fc05a387c55128c637f3d1028ce30e3451e7c30
Yoon321 <tkdwls734@gmail.com>
2024-02-22 23:18:53 +0900

    feat : 상품 생성 테스트 코드 수정
    각각의 테스트가 독립적으로 작동시키기 위해 "tearDown()" 코드에 repository 를 비우도록 하였다.

--- src/test/java/com/example/kioskapp/api/service/product/ProductServiceTest.java
+++ src/test/java/com/example/kioskapp/api/service/product/ProductServiceTest.java
@@ -4,6 +4,7 @@ import com.example.kioskapp.api.service.product.request.ProductCreateRequest;
 import com.example.kioskapp.api.service.product.response.ProductResponse;
 import com.example.kioskapp.domain.product.Product;
 import com.example.kioskapp.domain.product.ProductRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,11 @@ class ProductServiceTest {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @AfterEach
+    void tearDown() {
+        productRepository.deleteAllInBatch();
+    }
 
     @DisplayName("신규 상품을 등록한다. 상품번호는 가장 최근 상품의 상품번호에서 1 증가한 값이다.")
     @Test
