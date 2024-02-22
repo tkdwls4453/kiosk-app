c1b44c32f6215bf5d54a30f0e14fb6a843ec129a
Yoon321 <tkdwls734@gmail.com>
2024-02-22 22:23:22 +0900

    feat : Repository 설계

---
+++ src/main/java/com/example/kioskapp/domain/orderProduct/OrderProductRepository.java
@@ -0,0 +1,6 @@
+package com.example.kioskapp.domain.orderProduct;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
+}
